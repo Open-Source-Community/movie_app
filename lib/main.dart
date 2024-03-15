ecbe5cbb45bff4ff7031fbf25b80b32483c52fe0
Kero-83 <kerosarwat83@gmail.com>
2024-03-15 19:11:59 +0200

    Add Bottom Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_final_fields, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:movie_app/screens/category_screen.dart';
 import 'package:movie_app/screens/home_screen.dart';
+import 'package:movie_app/screens/settings_screen.dart';
+import 'package:movie_app/screens/wishlist_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// movie screen
+// categories
+// favorite
+// settings
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,7 +24,68 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Movie App',
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: MovieApp(),
+    );
+  }
+}
+
+class MovieApp extends StatefulWidget {
+  const MovieApp({super.key});
+
+  @override
+  State<MovieApp> createState() => _MovieAppState();
+}
+
+class _MovieAppState extends State<MovieApp> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgets = <Widget>[
+    HomeScreen(),
+    CategoryScreen(),
+    WishlistScreen(),
+    SettingsScreen()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: _widgets.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+            // backgroundColor: Colors.white,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: "Category",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "WishList",
+            // backgroundColor: Colors.white,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+            // backgroundColor: Colors.white,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
