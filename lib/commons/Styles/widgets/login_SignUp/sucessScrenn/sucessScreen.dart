0970e413082697e2ea8c0b9f9efc1edb61f5f14e
mohamed barakat <mohmedbarakat242@gmail.com>
2024-04-08 22:06:56 +0200

    firebase_signUp

--- lib/commons/Styles/widgets/login_SignUp/sucessScrenn/sucessScreen.dart
+++ lib/commons/Styles/widgets/login_SignUp/sucessScrenn/sucessScreen.dart
@@ -9,49 +9,70 @@ import '../../../../../utils/constants/image_strings.dart';
 import '../../../../../utils/constants/sizes.dart';
 
 class SucessScreen extends StatelessWidget {
-  const SucessScreen({super.key, required this.image, required this.title, required this.subtitle, required this.onpressed});
+  const SucessScreen(
+      {super.key,
+      required this.image,
+      required this.title,
+      required this.subtitle,
+      required this.onpressed});
 
-  final String image ,title,subtitle;
+  final String image, title, subtitle;
   final VoidCallback onpressed;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Padding(padding: MySpaceStyle.paddingWithAppBarHight*2
-        ,
+        child: Padding(
+          padding: MySpaceStyle.paddingWithAppBarHight * 2,
           child: Column(
             children: [
               //image
-<<<<<<< HEAD
-              Image(image:  AssetImage(image),width: MyHelperFunctions.screenWidth()*0.6,),
-=======
-              Image(image:  AssetImage(image),width: MyHelperFunctions.screenWidth()*0.6,height:MyHelperFunctions.screenHeight()*0.3),
->>>>>>> 214d749 (firebase_signUp)
+
+              Image(
+                image: AssetImage(image),
+                width: MyHelperFunctions.screenWidth() * 0.6,
+              ),
+
+              Image(
+                  image: AssetImage(image),
+                  width: MyHelperFunctions.screenWidth() * 0.6,
+                  height: MyHelperFunctions.screenHeight() * 0.3),
+
               const SizedBox(
                 height: MySizes.spaceBtwSections,
               ),
               //title &sub
-              Text(title,style: Theme.of(context).textTheme.headlineMedium,textAlign: TextAlign.center,),
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
               const SizedBox(
                 height: MySizes.spaceBtwItems,
               ),
-              Text("mofsifjsofsf.com",style: Theme.of(context).textTheme.labelLarge,textAlign: TextAlign.center,),
-              Text(subtitle,style: Theme.of(context).textTheme.labelMedium,textAlign: TextAlign.center,),
+              Text(
+                "mofsifjsofsf.com",
+                style: Theme.of(context).textTheme.labelLarge,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                subtitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
               const SizedBox(
                 height: MySizes.spaceBtwItems,
               ),
               //button
               SizedBox(
                 width: double.infinity,
-                child: ElevatedButton(onPressed: onpressed,child:  Text(MyTexts.Continue)),
+                child: ElevatedButton(
+                    onPressed: onpressed, child: Text(MyTexts.Continue)),
               ),
-
             ],
           ),
-
         ),
-
       ),
     );
   }
